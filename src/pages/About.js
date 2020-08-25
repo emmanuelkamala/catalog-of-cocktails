4be6a948884530951711c181b651a00301231ed5
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-26 00:20:34 +0300

    Add about page component to the app

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,11 @@
 import React from "react";
+import classes from './About.module.css';
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className={classes.about}>
+      <h1 className={classes.paragraph}>About Catalog of Cocktails</h1>
+      <p>I love mixing drinks and drinking them</p>
+    </section>
+  )
 }
