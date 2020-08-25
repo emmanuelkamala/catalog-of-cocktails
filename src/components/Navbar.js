70accd82c65fbf21ddcb1f5f6eeece567a563adf
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-26 00:05:07 +0300

    Add navbar component and styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
+import classes from './Navbar.module.css';
 
 export default function Navbar() {
-  return <h1>navbar component</h1>;
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.center}>
+        <img src={logo} alt='logo' className={classes.logo} />
+        <ul className={classes.links}>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
