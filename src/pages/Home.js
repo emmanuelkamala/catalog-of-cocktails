77b18400cefc5c3db3aedd65f7154fe17c435f1f
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-26 01:16:35 +0300

    Set up API and useEffect hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import CocktailsList from '../components/CocktailList';
 import SearchForm from '../components/SearchForm';
 
@@ -6,6 +6,13 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState('a');
   const [cocktail, setCocktail] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <main>
       <SearchForm setSearchTerm={setSearchTerm} />
