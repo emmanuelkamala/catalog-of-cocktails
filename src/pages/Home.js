ebca0f1571c17c17a9685bfa57c334fb598380a8
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-26 01:57:49 +0300

    Add functionality to get cocktails from the API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,33 @@ export default function Home() {
   const [cocktail, setCocktail] = useState([]);
 
   useEffect(() => {
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-    .then(response => response.json())
-    .then(data => setCocktail(data.drinks))
+    setLoading(true);
+    async function getDrinks() {
+      try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`);
+        const data = await response.json();
+        const {drinks} = data;
+        if (drinks) {
+          const newCocktail = drinks.map(item => {
+            const {idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass} = item;
+            return {
+              id:idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass
+            }
+          })
+          setCocktail(newCocktail);
+        } else {
+          setCocktail([])
+        }
+      } catch(error){
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
   }, [searchTerm])
 
   return (
