356105f71da6459ec3e1d9b77ed21ae1d52daf18
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-08-26 00:53:12 +0300

    Style the error page component

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,8 @@ import classes from './About.module.css';
 
 export default function About() {
   return (
-    <section className={classes.about}>
-      <h1 className={classes.paragraph}>About Catalog of Cocktails</h1>
+    <section className={`${classes.about} ${classes.section}`}>
+      <h1 className={classes.title}>About Catalog of Cocktails</h1>
       <p>I love mixing drinks and drinking them</p>
     </section>
   )
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import classes from './Error.module.css';
 
 export default function Error() {
-  return <h1>error page</h1>;
+  return (
+    <section className={`${classes.error} ${classes.section}`}>
+      <div className={classes.container}>
+        <h1>Oops! It's a dead end!</h1>
+        <Link to='/' className={`${classes.btn} ${classes.btnPrimary}`}>Back to Home</Link>
+      </div>
+    </section>
+  );
 }
